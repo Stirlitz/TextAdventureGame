929e39e59bd96fa27ad087cbf0f9d0847356f54b
Eric Mikulin <ericm@Erics-MacBook-Pro.local>
2014-06-14 20:12:15 -0600

    Inventory now has Stuff
    Wrote the first version of the inventory + Management bits.

--- src/adventure/Inventory.java
+++ src/adventure/Inventory.java
@@ -3,6 +3,28 @@ package adventure;
 public class Inventory {
 	
 	public Inventory(int size){
-		
+		inv = new Item[size];
 	}
+	
+	public boolean removeItem(Item removeItem){
+		for(int count = 0; count < inv.length; count++){
+			if(inv[count] == removeItem){
+				inv[count] = null;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean placeItem(Item placeingItem){
+		for(int count = 0; count < inv.length; count++){
+			if(!(inv[count] != null)){
+				inv[count] = placeingItem;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private static Item inv[];
 }
